fe1bb5c1f46f4fd5ab5d9700d75d4b47d952136e
Morgan Robertson <mrgnr@pm.me>
2020-06-22 22:33:37 +0200

    Add templates in MANIFEST.in.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='camus-chat',
-    version='0.1.dev1',
+    version='0.1.dev2',
     description='Peer-to-peer video chat using WebRTC',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
