06a2686fab92e2b97b8c1201622d84b68fc2af4b
Morgan Robertson <mrgnr@pm.me>
2020-06-22 13:52:44 +0200

    Update setup.py, add MANIFEST.in.

--- setup.py
+++ setup.py
@@ -2,7 +2,30 @@ from setuptools import setup, find_packages
 
 setup(
     name='camus-chat',
-    version='0.1dev',
-    packages=find_packages(),
+    version='0.1.dev0',
+    description='Peer-to-peer video chat using WebRTC',
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/mrgnr/rtc-chat',
+    author='Morgan Robertson',
+    license='GPL-3.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'aiortc==0.9.28',
+        'quart==0.11.5',
+        'bootstrap-flask==1.3.1',
+        'flask-wtf==0.14.3',
+        'pyee==7.0.2',
+        'python-slugify==4.0.0',
+        'werkzeug==1.0.1',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Environment :: Web Environment',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Communications :: Conferencing',
+        'Topic :: Multimedia :: Video',
+    ],
 )
