37b7a0d24d94393c70d3df0279df0042ff3dba9f
Morgan Robertson <mrgnr@pm.me>
2020-08-26 14:54:30 +0200

    Bump version to 0.1.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='camus-chat',
-    version='0.1.0',
+    version='0.1.1',
     description='Peer-to-peer video chat using WebRTC',
     long_description=open('README.rst').read(),
     long_description_content_type="text/x-rst; charset=UTF-8",
